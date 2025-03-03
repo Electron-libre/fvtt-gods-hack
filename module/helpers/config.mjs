5253b5dedd48be5d7c277e7e062923e2d61abf6d
Cedric <cedric.brancourt@gmail.com>
2025-03-03 17:50:26 +0100

    init

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const GODS_HACK = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+GODS_HACK.abilities = {
+  str: 'GODS_HACK.Ability.Str.long',
+  dex: 'GODS_HACK.Ability.Dex.long',
+  con: 'GODS_HACK.Ability.Con.long',
+  int: 'GODS_HACK.Ability.Int.long',
+  wis: 'GODS_HACK.Ability.Wis.long',
+  cha: 'GODS_HACK.Ability.Cha.long',
+};
+
+GODS_HACK.abilityAbbreviations = {
+  str: 'GODS_HACK.Ability.Str.abbr',
+  dex: 'GODS_HACK.Ability.Dex.abbr',
+  con: 'GODS_HACK.Ability.Con.abbr',
+  int: 'GODS_HACK.Ability.Int.abbr',
+  wis: 'GODS_HACK.Ability.Wis.abbr',
+  cha: 'GODS_HACK.Ability.Cha.abbr',
+};
